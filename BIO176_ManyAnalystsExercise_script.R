94426f6a0bd11dcf7b61197c2abfeef60402ab70
eucarida <olle.heyman@outlook.com>
2025-04-23 16:51:03 +0200

    Save from R we are soooo close... maby

--- BIO176_ManyAnalystsExercise_script.R
+++ BIO176_ManyAnalystsExercise_script.R
@@ -253,7 +253,7 @@ df_blue_dNA <- df_blue_dNA %>%
 
 
 # mixed models ####
-gblue_dNA.mixed <- lmer(day_14_weight ~ 
+mblue_dNA.mixed <- lmer(day_14_weight ~ 
                           rear_Cs_at_start_of_rearing + 
                           chicks_lost_percent +
                           hatch_year +
@@ -268,8 +268,303 @@ gblue_dNA.mixed <- lmer(day_14_weight ~
                         data = df_blue_dNA)
 
 
-check_model(gblue_dNA.mixed)
+check_model(mblue_dNA.mixed)
 #--- oooo so much collinearity
-summary(gblue_dNA.mixed)
+summary(mblue_dNA.mixed)
 #--- home-or _away does not seam to be that great of a effect,
-#--- 
+#--- we would like to center date of day14
+
+# center date of day 14
+df_blue_dNA <- df_blue_dNA %>% 
+  mutate(cen_Date_of_day14 = Date_of_day14 - 
+                                mean(df_blue_dNA$Date_of_day14, 
+                                     na.rm = TRUE))
+
+# v.1.2
+mblue_dNA.mixed2 <- lmer(day_14_weight ~ 
+                          rear_Cs_at_start_of_rearing + 
+                          chicks_lost_percent +
+                          hatch_year +
+                          cen_Date_of_day14 +
+                          hatch_year:cen_Date_of_day14 +
+                          rear_Cs_at_start_of_rearing:chicks_lost_percent +
+                          (1|hatch_mom_Ring) +
+                          (1|rear_area) +
+                          (1|rear_mom_Ring) +
+                          (1|home_or_away) +
+                          (1|net_rearing_manipulation),
+                        data = df_blue_dNA)
+
+check_model(mblue_dNA.mixed2)
+
+summary(mblue_dNA.mixed2)
+
+df_blue_dNA <- df_blue_dNA
+  mutate(chicks_lost_percent = chicks_lost_percent/100)
+
+check_model(mblue_dNA.mixed2,
+            check = "vif",
+            text = element_text(size = 3))
+
+
+df_blue_dNA %>% 
+  ggplot(aes(x = chicks_lost_percent)) +
+  geom_histogram()
+
+df_blue_dNA %>% 
+  ggplot(aes(x = log(chicks_lost_percent + 1))) +
+  geom_histogram()
+
+
+# v.1.3 we remove chicks lost rear Cs interaction
+mblue_dNA.mixed3 <- lmer(day_14_weight ~ 
+                           rear_Cs_at_start_of_rearing + 
+                           chicks_lost_percent +
+                           hatch_year +
+                           cen_Date_of_day14 +
+                           hatch_year:cen_Date_of_day14 +
+                           (1|hatch_mom_Ring) +
+                           (1|rear_area) +
+                           (1|rear_mom_Ring) +
+                           (1|home_or_away) +
+                           (1|net_rearing_manipulation),
+                         data = df_blue_dNA)
+
+check_model(mblue_dNA.mixed3,
+            check = "vif")
+summary(mblue_dNA.mixed3)
+
+# v.1.4 we remove chicks lost rear Cs interaction
+mblue_dNA.mixed4 <- lmer(day_14_weight ~ 
+                           rear_Cs_at_start_of_rearing + 
+                           chicks_lost_percent +
+                           hatch_year +
+                           cen_Date_of_day14 +
+                           (1|hatch_mom_Ring) +
+                           (1|rear_area) +
+                           (1|rear_mom_Ring) +
+                           (1|home_or_away) +
+                           (1|net_rearing_manipulation),
+                         data = df_blue_dNA)
+
+check_model(mblue_dNA.mixed4,
+            check = "vif")
+check_model(mblue_dNA.mixed4,
+            check = "outliers")
+summary(mblue_dNA.mixed4)
+
+#--- after discus ions we deside that his is the mainf 
+
+#--- after discussing the vif 
+
+
+# anova
+
+mblue_dNA.mixed5 <- update(mblue_dNA.mixed4,
+                           ~. - (1|home_or_away))
+
+anova(mblue_dNA.mixed5,
+      mblue_dNA.mixed4,
+      test = "Chisq")
+
+AIC(mblue_dNA.mixed,
+    mblue_dNA.mixed2,
+    mblue_dNA.mixed3,
+    mblue_dNA.mixed4,
+    mblue_dNA.mixed5)
+
+mblue_dNA.mixed4.2 <- update(mblue_dNA.mixed4,
+                           ~. - (1|rear_area))
+
+anova(mblue_dNA.mixed4.2,
+      mblue_dNA.mixed4,
+      test = "Chisq")
+
+mblue_dNA.mixed4.3 <- update(mblue_dNA.mixed4,
+                             ~. - (1|net_rearing_manipulation))
+
+anova(mblue_dNA.mixed4.3,
+      mblue_dNA.mixed4,
+      test = "Chisq")
+
+sum(is.na(df_blue_dNA$net_rearing_manipulation))
+
+
+df_blue_dNA %>% 
+  filter(is.na(df_blue_dNA$net_rearing_manipulation)) %>% 
+  group_by(net_rearing_manipulation) %>% 
+  summarise(d14 = d14_rear_nest_brood_size,
+            d2 = rear_Cs_at_start_of_rearing,
+            ID = rear_nest_breed_ID,
+            DidrID = chick_ring_number)
+
+df_blue_dNA %>% 
+  summarise(across(everything(), ~ sum(is.na(.)))) %>% 
+  str()
+
+# more filtering out NA from the data set as it seams to disrupt the anova
+df_blue_dNA <- df_blue_dNA %>% 
+  drop_na(net_rearing_manipulation,
+          hatch_mom_Ring,
+          rear_mom_Ring)
+
+
+
+sum(is.na(df_blue_dNA$net_rearing_manipulation))
+sum(is.na(df_blue_dNA$hatch_mom_Ring))
+sum(is.na(df_blue_dNA$rear_mom_Ring))
+#--- no more NA in the model, yipi
+
+## A redo of what is above but with removed NA ####
+
+# v.1.4 we remove chicks lost rear Cs interaction
+blue_dNA.mixed.t1 <- lmer(day_14_weight ~ 
+                           rear_Cs_at_start_of_rearing + 
+                           chicks_lost_percent +
+                           hatch_year +
+                           cen_Date_of_day14 +
+                           (1|hatch_mom_Ring) +
+                           (1|rear_area) +
+                           (1|rear_mom_Ring) +
+                           (1|home_or_away) +
+                           (1|net_rearing_manipulation),
+                         data = df_blue_dNA)
+
+check_model(blue_dNA.mixed.t1)
+check_model(blue_dNA.mixed.t1,
+            check = "vif")
+check_model(blue_dNA.mixed.t1,
+            check = "outliers")
+summary(blue_dNA.mixed.t1)
+
+#--- after discus ions we deside that his is the mainf 
+
+#--- after discussing the vif 
+
+
+# anova time
+blue_dNA.mixed.t2 <- update(blue_dNA.mixed.t1,
+                           ~. - (1|home_or_away))
+
+anova(blue_dNA.mixed.t1,
+      blue_dNA.mixed.t2,
+      test = "Chisq")
+#--- t1 is better (p-vaue = ***)
+
+blue_dNA.mixed.t3 <- update(blue_dNA.mixed.t1,
+                             ~. - (1|rear_area))
+
+anova(blue_dNA.mixed.t3,
+      blue_dNA.mixed.t1,
+      test = "Chisq")
+#--- t1 is sightly better
+
+blue_dNA.mixed.t4 <- update(blue_dNA.mixed.t1,
+                             ~. - (1|net_rearing_manipulation))
+
+anova(blue_dNA.mixed.t4,
+      blue_dNA.mixed.t1,
+      test = "Chisq")
+#--- t1 is better
+
+blue_dNA.mixed.t5 <- update(blue_dNA.mixed.t1,
+                            ~. - (1|hatch_mom_Ring))
+
+anova(blue_dNA.mixed.t5,
+      blue_dNA.mixed.t1,
+      test = "Chisq")
+#--- t1 is better
+
+blue_dNA.mixed.t6 <- update(blue_dNA.mixed.t1,
+                            ~. - (1|rear_mom_Ring))
+
+anova(blue_dNA.mixed.t6,
+      blue_dNA.mixed.t1,
+      test = "Chisq")
+#--- t1 is better
+#--- we assume that by this metric (and the once before) that the t1 model is the best fit for the mixed model...
+
+summary(blue_dNA.mixed.t1)
+
+
+# Vart är vi påväg?....
+# För 10 poäng...
+
+# new data 1
+df_new_blue_dNA.1 <-
+  crossing(
+    rear_Cs_at_start_of_rearing =
+      seq(
+        min(df_blue_dNA$rear_Cs_at_start_of_rearing),
+        max(df_blue_dNA$rear_Cs_at_start_of_rearing),
+        length = 100
+      ),
+    chicks_lost_percent =
+      rep(
+        min(df_blue_dNA$chicks_lost_percent),
+        max(df_blue_dNA$chicks_lost_percent),
+        times = 100
+      ),
+    cen_Date_of_day14 =
+      rep(
+        min(df_blue_dNA$cen_Date_of_day14),
+        max(df_blue_dNA$cen_Date_of_day14),
+        times = 100
+      ),
+    hatch_year = c(2001, 2002, 2003))
+
+
+
+# new data 2 
+df_new_blue_dNA.2 <-
+  tidyr::expand(
+    df_blue_dNA,
+    rear_Cs_at_start_of_rearing =
+      seq(
+        min(df_blue_dNA$rear_Cs_at_start_of_rearing),
+        max(df_blue_dNA$rear_Cs_at_start_of_rearing),
+        length = 100
+      ),
+    chicks_lost_percent =
+      rep(
+        mean(df_blue_dNA$chicks_lost_percent),
+        times = 100
+      ),
+    cen_Date_of_day14 =
+      rep(
+        mean(df_blue_dNA$cen_Date_of_day14),
+        times = 100
+      ),
+    hatch_year = c(2001, 2002, 2003),
+    nesting(
+      hatch_mom_Ring,
+      rear_mom_Ring,
+      net_rearing_manipulation,
+      rear_area,
+      home_or_away
+    )
+  )
+#--- this is our first test
+
+
+# for reffrensing our old work
+# df_newgrouse3 <- 
+#   tidyr::expand(df_grouse, 
+#                 cen_log_totalcount = seq(min(df_grouse$cen_log_totalcount),
+#                                          max(df_grouse$cen_log_totalcount),
+#                                          length = 100), 
+#                 cen_prev = c(-1,0,1),
+#                 nesting(moor, drive))
+
+# predictInterval(g3.mixed, 
+#                 newdata = df_newgrouse3, 
+#                 level = 0.8,
+#                 n.sims = 100)
+
+blue_pred_dNA.1 <- predictInterval(blue_dNA.mixed.t1,
+                                   newdata = df_new_blue_dNA.2,
+                                   level = 0.8,
+                                   n.sims = 1000)
+str(df_blue_dNA)    
+                            
+
